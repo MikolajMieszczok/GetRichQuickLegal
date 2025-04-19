e5fb29c74a9bb40a749f9a41c95472455dd42ecc
TheOathbringer <115071107+MikolajMieszczok@users.noreply.github.com>
2025-04-19 19:24:19 +0200

    Add files via upload

--- StockDatatoExcel/main.py
+++ StockDatatoExcel/main.py
@@ -2,30 +2,22 @@ import datetime
 import xlsxwriter
 import yfinance as yf
 import openpyxl
+import xlsxwriter
 from datetime import datetime, timedelta
-# def stock_price_over_time(row, ws, worksheet):
-#     ticker = ws["C" + row].value.split(".")[0]
-#     start_date = datetime.datetime.strptime(ws["F" + row].value, "%Y-%m-%d").date()
-#     end_date = datetime.datetime.strptime(ws["H" + row].value, "%Y-%m-%d").date()
-#     hist = yf.Ticker(ticker).history(start=start_date, end=end_date, interval="1d")
-#     if not hist.empty:
-#         last_close = hist["Close"].iloc[-1]  # Get the last available closing price
-#     else:
-#         last_close = "N/A"  # Or handle it as needed
-#     worksheet.write("I" + row, last_close)
-columns = ['H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+
+columns = ['E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
  'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ', 'AK', 'AL', 'AM', 'AN', 'AO',
- 'AP', 'AQ', 'AR', 'AS', 'AT', 'AU', 'AV', 'AW', 'AX', 'AY', 'AZ', 'BA', 'BB', 'BC', 'BD',
- 'BE', 'BF', 'BG', 'BH', 'BI', 'BJ', 'BK', 'BL', 'BM', 'BN', 'BO', 'BP', 'BQ']
-workbook = xlsxwriter.Workbook('PRAWIEGOTOWE.xlsx')
+ 'AP', 'AQ', 'AR', 'AS', 'AT', 'AU']
+workbook = xlsxwriter.Workbook("C:/Users/Mikołaj/Desktop/Enginer/SECONDEXCEL.xlsx")
 worksheet = workbook.add_worksheet()
-wb = openpyxl.load_workbook("STOCKS-DATE.xlsx")
+wb = openpyxl.load_workbook("C:/Users/Mikołaj/Desktop/Enginer/FIRSTEXCEL.xlsx")
 ws = wb.active
-for row in range(12000, 14507):
+max_row = 0
+for row in range(1, 30):
     try:
         print(row)
         row = str(row)
-        not_filtered = yf.Ticker(str(ws["B" + row].value).split(".")[0]).history(start = str(((ws["D" + row]).value)).split(" ")[0], end = str(((ws["C" + row]).value)).split(" ")[0], interval = "1d")
+        not_filtered = yf.Ticker(str(ws["B" + row].value).split(".")[0]).history(start = str(((ws["E" + row]).value)).split(" ")[0], end = str(((ws["D" + row]).value)).split(" ")[0], interval = "1d")
         #print(not_filtered)
         if not_filtered.empty:
             continue
@@ -35,4 +27,36 @@ for row in range(12000, 14507):
     except Exception as e:
         print(f"Error at row {row}: {e}")
 workbook.close()
-#15238
+wb_third = openpyxl.load_workbook("C:/Users/Mikołaj/Desktop/Enginer/FIRSTEXCEL.xlsx")
+ws_third = wb_third.active
+workbook = xlsxwriter.Workbook("C:/Users/Mikołaj/Desktop/Enginer/THIRDEXCEL.xlsx")
+worksheet = workbook.add_worksheet()
+wb_second = openpyxl.load_workbook("C:/Users/Mikołaj/Desktop/Enginer/SECONDEXCEL.xlsx")
+ws_second = wb_second.active
+dict_people = {}
+dict_stocks = {}
+wb_stocks = openpyxl.load_workbook("C:/Users/Mikołaj/Desktop/Enginer/IdPeopleStocks.xlsx")
+ws_stocks = wb_stocks.active
+for row in range(1, 15230):
+    dict_people[str(ws_stocks["C" + str(row)].value)] = ws_stocks["A" + str(row)].value
+    dict_stocks[str(ws_stocks["D" + str(row)].value)] = ws_stocks["B" + str(row)].value
+for row in range(1, 30):
+   if str(datetime.now().strftime("%Y-%m-%d")) != str(ws_third["D" + str(row)].value):
+        max_row = 11
+        break
+max_row = int(max_row)
+max_row = max_row - 1
+for x in range(1, max_row):
+    new_d_p = {v: k for k, v in dict_people.items()}
+    key_p = new_d_p.get(str(ws_third["A" + str(x)].value))
+    print(key_p)
+    division = ws_second["E" + str(x)].value
+    if key_p != None:
+        worksheet.write("A" + str(x), str(key_p))
+        new_d_s = {v: k for k, v in dict_stocks.items()}
+        key_s = new_d_s.get(str(ws_third["B" + str(x)].value))
+        worksheet.write("B" + str(x), str(key_s))
+        worksheet.write("C" + str(x), str(ws_third["C" + str(x)].value))
+        for y in range(len(columns)-1):
+            worksheet.write(str(columns[y]) + str(x), str(ws_second[str(columns[y+1] + str(x))].value/division))
+workbook.close()
